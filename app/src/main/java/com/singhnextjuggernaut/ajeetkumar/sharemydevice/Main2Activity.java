fdeb94a70fbfa8b66f0e02a3c522d7cbd1b209e1
Rahul Kumar <rahulkumar@rediffmail.com>
2018-03-18 15:45:02 +0530

    rebased

---
+++ app/src/main/java/com/singhnextjuggernaut/ajeetkumar/sharemydevice/Main2Activity.java
@@ -0,0 +1,15 @@
+package com.singhnextjuggernaut.ajeetkumar.sharemydevice;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+// /Devloped by Ajeet singh
+
+public class Main2Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main2);
+    }
+}
---
+++ app/src/main/java/com/singhnextjuggernaut/ajeetkumar/sharemydevice/MainActivity.java
@@ -0,0 +1,43 @@
+package com.singhnextjuggernaut.ajeetkumar.sharemydevice;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+
+//Devloped by Ajeet singh
+
+public class MainActivity extends AppCompatActivity {
+    private Button login;
+    private Button register;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        login = (Button) findViewById(R.id.buttonLogin);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, Main2Activity.class);
+                startActivity(intent);
+
+            }
+        });
+        register = (Button) findViewById(R.id.buttonRegister);
+        register.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, Register.class);
+                startActivity(intent);
+
+            }
+        });
+    }
+
+    }
+
+
