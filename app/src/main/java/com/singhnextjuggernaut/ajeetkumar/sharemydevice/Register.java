6798f98601219385b4db098c11423e670eaf33af
Rahul Kumar <rahulkumar@rediffmail.com>
2018-03-23 10:38:41 +0530

    utils added

--- app/src/main/java/com/singhnextjuggernaut/ajeetkumar/sharemydevice/Register.java
+++ app/src/main/java/com/singhnextjuggernaut/ajeetkumar/sharemydevice/Register.java
@@ -2,12 +2,30 @@ package com.singhnextjuggernaut.ajeetkumar.sharemydevice;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.singhnextjuggernaut.ajeetkumar.sharemydevice.Validations.Utils;
 
 public class Register extends AppCompatActivity {
-
+    private EditText name,email,password,confirm_password;
+    private Button submit;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        name = (EditText) findViewById(R.id.registerName);
+        email = (EditText) findViewById(R.id.registerEmail);
+        password = (EditText) findViewById(R.id.registerPassword);
+        confirm_password = (EditText) findViewById(R.id.confregPassword);
+        submit = (Button) findViewById(R.id.registerButton);
+
+        submit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            }
+        });
+
     }
 }
---
+++ app/src/main/java/com/singhnextjuggernaut/ajeetkumar/sharemydevice/Validations/Utils.java
@@ -0,0 +1,24 @@
+package com.singhnextjuggernaut.ajeetkumar.sharemydevice.Validations;
+
+/**
+ * Created by rahul on 23/3/18.
+ */
+
+public class Utils {
+
+    public static boolean chkLength(String text,int minLength) {
+        Boolean check=true;
+        if (text.length() < minLength) {
+            check=false;
+        }
+        return check;
+    }
+
+    public static  boolean matchPassword(String s1, String s2) {
+        Boolean check = true;
+        if(s1.compareTo(s1) !=0) {
+            check=false;
+        }
+        return check;
+    }
+}
