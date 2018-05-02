838121f183239cbc42d0bb41be08752d224bcade
Ajeet Singh <ajeet.ajeet.aks@gmail.com>
2018-05-03 01:27:26 +0530

    adapter for ios and cable

--- app/src/main/java/com/singhnextjuggernaut/ajeetkumar/sharemydevice/IosFragment.java
+++ app/src/main/java/com/singhnextjuggernaut/ajeetkumar/sharemydevice/IosFragment.java
@@ -3,9 +3,16 @@ package com.singhnextjuggernaut.ajeetkumar.sharemydevice;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+
+import com.singhnextjuggernaut.ajeetkumar.sharemydevice.data.DeviceData;
+import com.singhnextjuggernaut.ajeetkumar.sharemydevice.database.CommonData;
+
+import java.util.List;
 
 
 /**
@@ -13,6 +20,8 @@ import android.view.ViewGroup;
  */
 public class IosFragment extends Fragment {
 
+    List<DeviceData> devices;
+    RecyclerView recyclerView;
 
     public IosFragment() {
         // Required empty public constructor
@@ -23,7 +32,26 @@ public class IosFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_ios, container, false);
+        View rootview=inflater.inflate(R.layout.fragment_ios, container, false);
+        //getting the recyclerview from xml
+        recyclerView = (RecyclerView) rootview.findViewById(R.id.ios_tab_recycler);
+        //recyclerView.addItemDecoration(new DividerItemDecoration(getActivity(), DividerItemDecoration.VERTICAL));
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+
+        //initializing the productlist
+        devices = CommonData.getAndroidList();
+
+        //creating recyclerview adapter
+        android_list_adapter adapter = new android_list_adapter(getActivity(),devices);
+
+        //setting adapter to recyclerview
+        recyclerView.setAdapter(adapter);
+
+
+        return rootview;
+        // Inflate the layout for this fragment
+       // return inflater.inflate(R.layout.fragment_ios, container, false);
     }
 
 }
