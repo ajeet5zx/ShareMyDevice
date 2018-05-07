42423086e01f39a835924a480a70afd733ae9840
Ajeet Singh <ajeet.ajeet.aks@gmail.com>
2018-05-07 15:23:08 +0530

    side menu on item selected implemented

---
+++ app/src/main/java/com/singhnextjuggernaut/ajeetkumar/sharemydevice/MyAndroidDevices.java
@@ -0,0 +1,13 @@
+package com.singhnextjuggernaut.ajeetkumar.sharemydevice;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MyAndroidDevices extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_my_android_devices);
+    }
+}
