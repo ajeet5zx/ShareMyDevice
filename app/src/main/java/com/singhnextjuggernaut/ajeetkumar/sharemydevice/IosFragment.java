e686ad7dfd9e58f1f436dfe2fa428ee82b5705fa
Ajeet Singh <ajeet.ajeet.aks@gmail.com>
2018-05-28 20:57:10 +0530

    Swipe down to refresh added in ios ad cables

--- app/src/main/java/com/singhnextjuggernaut/ajeetkumar/sharemydevice/IosFragment.java
+++ app/src/main/java/com/singhnextjuggernaut/ajeetkumar/sharemydevice/IosFragment.java
@@ -3,16 +3,25 @@ package com.singhnextjuggernaut.ajeetkumar.sharemydevice;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.singhnextjuggernaut.ajeetkumar.sharemydevice.constant.AppConstant;
 import com.singhnextjuggernaut.ajeetkumar.sharemydevice.data.DeviceData;
 import com.singhnextjuggernaut.ajeetkumar.sharemydevice.database.CommonData;
+import com.singhnextjuggernaut.ajeetkumar.sharemydevice.retrofit.ApiCaller;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 
 /**
@@ -22,6 +31,7 @@ public class IosFragment extends Fragment {
 
     List<DeviceData> devices;
     RecyclerView recyclerView;
+    SwipeRefreshLayout mSwipeRefreshLayout;
 
     public IosFragment() {
         // Required empty public constructor
@@ -34,8 +44,108 @@ public class IosFragment extends Fragment {
         // Inflate the layout for this fragment
         View rootview=inflater.inflate(R.layout.fragment_ios, container, false);
         //getting the recyclerview from xml
-        recyclerView = (RecyclerView) rootview.findViewById(R.id.ios_tab_recycler);
+        recyclerView = rootview.findViewById(R.id.ios_tab_recycler);
         //recyclerView.addItemDecoration(new DividerItemDecoration(getActivity(), DividerItemDecoration.VERTICAL));
+
+
+        mSwipeRefreshLayout = rootview.findViewById(R.id.swipeToRefreshIos);
+        mSwipeRefreshLayout.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            public void onRefresh() {
+                Call<List<DeviceData>> device_list_api = ApiCaller.getApiInterface().devicelist(CommonData.getAccessToken());
+                device_list_api.enqueue(new Callback<List<DeviceData>>() {
+                    @Override
+                    public void onResponse(Call<List<DeviceData>> call, Response<List<DeviceData>> response) {
+                        if (response.isSuccessful()) {
+                            ArrayList<DeviceData> android, ios, cable;
+                            android = new ArrayList<>();
+                            ios = new ArrayList<>();
+                            cable = new ArrayList<>();
+                            for (int i = 0; i < response.body().size(); i++) {
+                                switch (response.body().get(i).getDeviceCategory()) {
+                                    case AppConstant.DEVICE_CATEGORY_ANDROID: {
+                                        android.add(response.body().get(i));
+                                        break;
+                                    }
+                                    case AppConstant.DEVICE_CATEGORY_IOS: {
+                                        ios.add(response.body().get(i));
+                                        break;
+                                    }
+                                    case AppConstant.DEVICE_CATEGORY_CABLE: {
+                                        cable.add(response.body().get(i));
+                                        break;
+                                    }
+                                }
+                            }
+                            CommonData.saveAndroidList(android);
+                            CommonData.saveIOSList(ios);
+                            CommonData.saveCableList(cable);
+                            recyclerView.removeAllViews();
+                            recyclerView.setHasFixedSize(true);
+                            recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+                            //initializing the productlist
+                            devices = CommonData.getIOSList();
+                            //creating recyclerview adapter
+                            android_list_adapter adapter = new android_list_adapter(getActivity(), devices);
+                            //setting adapter to recyclerview
+                            adapter.notifyDataSetChanged();
+                            recyclerView.setAdapter(adapter);
+                            recyclerView.invalidate();
+                            //Log.d("BODYSIZE","BODYSIZE : "+response.body().size());
+                            //Log.d("COUNT","ADDROID:"+CommonData.getAndroidList().size()+" IOS:"+CommonData.getIOSList().size()+" Cable:"+CommonData.getCableList().size());
+
+                        } else {
+
+                        }
+
+
+//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
+                        // Create the adapter that will return a fragment for each of the three
+                        // primary sections of the activity.
+//                        mSectionsPagerAdapter = new home.SectionsPagerAdapter(getSupportFragmentManager());
+//
+//                        // Set up the ViewPager with the sections adapter.
+//                        mViewPager = (ViewPager) findViewById(R.id.container);
+//                        mViewPager.setAdapter(mSectionsPagerAdapter);
+//
+//                        TabLayout tabLayout = (TabLayout) findViewById(R.id.tabs);
+//
+//                        mViewPager.addOnPageChangeListener(new TabLayout.TabLayoutOnPageChangeListener(tabLayout));
+//                        tabLayout.addOnTabSelectedListener(new TabLayout.ViewPagerOnTabSelectedListener(mViewPager));
+
+                    }
+
+                    @Override
+                    public void onFailure(Call<List<DeviceData>> call, Throwable t) {
+                        Log.d("err", t.getMessage());
+                    }
+                });
+
+                mSwipeRefreshLayout.setRefreshing(false);
+            }
+        });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
 
